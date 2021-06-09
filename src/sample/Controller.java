7b8b0632b1e934535ff7208cfbf6940541ec239f
Dileesha Weliwaththa <64683688+dileeshaweliwaththa@users.noreply.github.com>
2021-06-09 15:50:47 +0530

    Calculator by JavaFX

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,62 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField n1;
+    @FXML
+    private TextField n2;
+    @FXML
+    private Label lb1;
+
+    public void addition(){
+        float num1= Float.parseFloat(n1.getText());
+        float num2= Float.parseFloat(n2.getText());
+        lb1.setText(String.valueOf(num1+num2));
+    }
+    
+    public void subtraction(){
+        float num1= Float.parseFloat(n1.getText());
+        float num2= Float.parseFloat(n2.getText());
+        lb1.setText(String.valueOf(num1-num2));
+    }
+
+    public void multiplication(){
+        float num1= Float.parseFloat(n1.getText());
+        float num2= Float.parseFloat(n2.getText());
+        lb1.setText(String.valueOf(num1*num2)
+        );
+    }
+
+    public void division(){
+        float num1= Float.parseFloat(n1.getText());
+        float num2= Float.parseFloat(n2.getText());
+        lb1.setText(String.valueOf(num1/num2));
+    }
+
+    //Number One TextField Only Taken Hereafter
+
+    public void calsin(){
+        float num1= Float.parseFloat(n1.getText());
+        lb1.setText(String.valueOf(Math.sin(num1)));
+    }
+
+    public void calcos(){
+        float num1= Float.parseFloat(n1.getText());
+        lb1.setText(String.valueOf(Math.cos(num1)));
+    }
+
+    public void caltan(){
+        float num1= Float.parseFloat(n1.getText());
+        lb1.setText(String.valueOf(Math.tan(num1)));
+    }
+
+    public void calex(){
+        float num1= Float.parseFloat(n1.getText());
+        lb1.setText(String.valueOf(Math.exp(num1)));
+    }
 }
